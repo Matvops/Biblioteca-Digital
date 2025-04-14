b03e5a9f105170a0c13f8e42760e157674f7cd0e
Matheus <matheusmartinscia@gmail.com>
2025-04-14 20:12:53 -0300

    Create seeders to category table and Books table

--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -18,6 +18,7 @@ class BooksTableSeeder extends Seeder
         DB::table('books')->insert([
             [
                 'author_id' => 1,
+                'category_id' => 1,
                 'title' => 'Clean Code',
                 'year' => 2008,
                 'readed' => true,
@@ -25,6 +26,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 1,
+                'category_id' => 1,
                 'title' => 'Clean Architecture',
                 'year' => 2017,
                 'readed' => false,
@@ -32,6 +34,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 1,
+                'category_id' => 1,
                 'title' => 'The Clean Coder',
                 'year' => 2011,
                 'readed' => true,
@@ -39,6 +42,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 1,
+                'category_id' => 1,
                 'title' => 'Agile Software Development',
                 'year' => 2002,
                 'readed' => false,
@@ -46,6 +50,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 1,
+                'category_id' => 1,
                 'title' => 'Clean Agile',
                 'year' => 2019,
                 'readed' => false,
@@ -53,6 +58,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 2,
+                'category_id' => 1,
                 'title' => 'Refactoring',
                 'year' => 1999,
                 'readed' => true,
@@ -60,6 +66,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 2,
+                'category_id' => 1,
                 'title' => 'Patterns of Enterprise Application Architecture',
                 'year' => 2002,
                 'readed' => false,
@@ -67,6 +74,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 2,
+                'category_id' => 1,
                 'title' => 'UML Distilled',
                 'year' => 2003,
                 'readed' => false,
@@ -74,6 +82,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 2,
+                'category_id' => 1,
                 'title' => 'Domain-Specific Languages',
                 'year' => 2010,
                 'readed' => false,
@@ -81,6 +90,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 2,
+                'category_id' => 1,
                 'title' => 'Continuous Integration',
                 'year' => 2006,
                 'readed' => false,
@@ -88,6 +98,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 3,
+                'category_id' => 1,
                 'title' => 'Domain-Driven Design',
                 'year' => 2003,
                 'readed' => false,
@@ -95,6 +106,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 4,
+                'category_id' => 1,
                 'title' => 'The Art of Computer Programming Vol. 1',
                 'year' => 1968,
                 'readed' => false,
@@ -102,6 +114,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 4,
+                'category_id' => 1,
                 'title' => 'The Art of Computer Programming Vol. 2',
                 'year' => 1969,
                 'readed' => false,
@@ -109,6 +122,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 4,
+                'category_id' => 1,
                 'title' => 'The Art of Computer Programming Vol. 3',
                 'year' => 1973,
                 'readed' => false,
@@ -116,6 +130,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 4,
+                'category_id' => 1,
                 'title' => 'The Art of Computer Programming Vol. 4A',
                 'year' => 2011,
                 'readed' => false,
@@ -123,6 +138,7 @@ class BooksTableSeeder extends Seeder
             ],   
             [
                 'author_id' => 5,
+                'category_id' => 1,
                 'title' => 'Code Complete',
                 'year' => 1993,
                 'readed' => true,
@@ -130,6 +146,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 5,
+                'category_id' => 1,
                 'title' => 'Rapid Development',
                 'year' => 1996,
                 'readed' => false,
@@ -137,6 +154,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 5,
+                'category_id' => 1,
                 'title' => 'Software Estimation',
                 'year' => 2006,
                 'readed' => false,
@@ -144,6 +162,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 6,
+                'category_id' => 2,
                 'title' => 'Novos Negócios Inovadores de Crescimento Empreendedor no Brasil',
                 'year' => 2013,
                 'readed' => false,
@@ -151,6 +170,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 6,
+                'category_id' => 2,
                 'title' => 'O que é estratégia digital?',
                 'year' => 2021,
                 'readed' => false,
@@ -158,6 +178,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 7,
+                'category_id' => 2,
                 'title' => 'Lógica de Programação: A construção de algoritmos e estruturas de dados',
                 'year' => 2012,
                 'readed' => false,
@@ -165,6 +186,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 8,
+                'category_id' => 2,
                 'title' => 'Ruby on Rails: Novas tendências para o desenvolvimento Ágil de Aplicações Web',
                 'year' => 2007,
                 'readed' => false,
@@ -172,6 +194,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 9,
+                'category_id' => 2,
                 'title' => 'HTML não é linguagem de programação',
                 'year' => 2015,
                 'readed' => false,
@@ -179,6 +202,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 10,
+                'category_id' => 2,
                 'title' => 'Código Limpo em Java',
                 'year' => 2022,
                 'readed' => true,
@@ -186,6 +210,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 10,
+                'category_id' => 2,
                 'title' => 'Testes Automatizados de Software',
                 'year' => 2019,
                 'readed' => false,
@@ -193,6 +218,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 11,
+                'category_id' => 3,
                 'title' => 'O Guia do Mochileiro das Galáxias',
                 'year' => 1979,
                 'readed' => true,
@@ -200,6 +226,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 11,
+                'category_id' => 3,
                 'title' => 'O Restaurante no Fim do Universo',
                 'year' => 1980,
                 'readed' => false,
@@ -207,13 +234,16 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 12,
+                'category_id' => 3,
                 'title' => 'Neuromancer',
+                'category_id' => '3',
                 'year' => 1984,
                 'readed' => false,
                 'image' => 'https://m.media-amazon.com/images/I/41LVxzmgcOL._SX325_BO1,204,203,200_.jpg'
             ],
             [
                 'author_id' => 13,
+                'category_id' => 3,
                 'title' => 'Eu, Robô',
                 'year' => 1950,
                 'readed' => true,
@@ -221,6 +251,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 13,
+                'category_id' => 3,
                 'title' => 'Fundação',
                 'year' => 1951,
                 'readed' => false,
@@ -228,6 +259,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 14,
+                'category_id' => 3,
                 'title' => '1984',
                 'year' => 1949,
                 'readed' => true,
@@ -235,6 +267,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 14,
+                'category_id' => 3,
                 'title' => 'A Revolução dos Bichos',
                 'year' => 1945,
                 'readed' => true,
@@ -242,6 +275,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 15,
+                'category_id' => 3,
                 'title' => 'Snow Crash',
                 'year' => 1992,
                 'readed' => false,
@@ -249,6 +283,7 @@ class BooksTableSeeder extends Seeder
             ],
             [
                 'author_id' => 15,
+                'category_id' => 3,
                 'title' => 'Cryptonomicon',
                 'year' => 1999,
                 'readed' => false,
--- database/seeders/MainSeeder.php
+++ database/seeders/MainSeeder.php
@@ -12,6 +12,6 @@ class MainSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([AuthorsTableSeeder::class, BooksTableSeeder::class]);
+        $this->call([CategoriesTableSeeder::class, AuthorsTableSeeder::class, BooksTableSeeder::class]);
     }
 }
