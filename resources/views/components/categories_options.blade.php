fea37e17591e1e9ca14a3416efbf384ebf264dfb
Matheus <matheusmartinscia@gmail.com>
2025-04-18 18:51:32 -0300

    Feat: Add toast

--- resources/views/components/categories_options.blade.php
+++ resources/views/components/categories_options.blade.php
@@ -1,4 +1,4 @@
-<select name="category" class="cursor-pointer bg-stone-200 pl-2 text-center rounded-sm shadow-black inset-shadow-2xs text-gray-700 font-semibold">
+<select name="category" class="cursor-pointer bg-stone-200 pr-6 text-center rounded-sm shadow-black inset-shadow-2xs text-gray-700 font-semibold">
     <option value="0" class="cursor-pointer">ALL CATEGORIES</option>
     @foreach($options as $option)
         <option value="{{ $option->id }}" class="cursor-pointer" >{{ $option->name }}</option>
