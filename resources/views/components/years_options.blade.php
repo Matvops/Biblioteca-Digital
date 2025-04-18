6b5b27737bed90623735d069ff010c8bf65d268b
Matheus <matheusmartinscia@gmail.com>
2025-04-18 11:26:30 -0300

    Fix: Pequenas correções nos componentes

--- resources/views/components/years_options.blade.php
+++ resources/views/components/years_options.blade.php
@@ -1,5 +1,5 @@
-<select name="category" class="cursor-pointer bg-stone-200 w-[10%] rounded-sm shadow-black inset-shadow-2xs pl-2 text-gray-700 font-semibold">
-    <option value="0" class="cursor-pointer" selected>ALL YEARS</option>
+<select name="year" class="cursor-pointer bg-stone-200 w-[10%] rounded-sm shadow-black inset-shadow-2xs pl-2 text-gray-700 font-semibold">
+    <option value="0" class="cursor-pointer">ALL YEARS</option>
     @foreach($options as $option)
         <option value="{{ $option->year }}" class="cursor-pointer" >{{ $option->year }}</option>
     @endforeach
