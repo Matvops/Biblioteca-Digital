3cdc05f19df6a0406462e1ee3652a9812f1d609a
Matheus <matheusmartinscia@gmail.com>
2025-04-19 13:10:21 -0300

    Feat: Create Controller method, to create new book

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\services\MainService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
+use Laravel\Pail\Files;
+
+use function Laravel\Prompts\error;
 
 class MainController extends Controller
 {
@@ -16,8 +20,30 @@ class MainController extends Controller
         $this->service = new MainService;
     }
 
-    public function home(Request $request)
+    public function home()
     {
+
+
+        $filters = [
+            'year' => 0,
+            'category' => 0,
+            'input' => null,
+        ];
+
+        $categories = $this->service->searchAllCategories();
+    
+        $years = $this->service->searchAllYears();
+
+        $books = $this->service->searchWithFilters($filters);
+
+        return view('home', [
+            "categories" => $categories,
+            "years" => $years,
+            "books" => $books->getData()
+        ]);
+    }
+
+    public function homeFilter(Request $request) {
         $request->validate(
             [
                 'year' => 'required',
@@ -55,5 +81,46 @@ class MainController extends Controller
             "years" => $years,
             "books" => $books->getData()
         ]);
+    }
+
+    public function create(){
+
+        $categories = $this->service->searchAllCategories();
+
+        return view('register', ['categories' => $categories]);
+    }
+
+    public function createSubmit(Request $request) {
+        $request->validate(
+            [
+                'title' => 'required',
+                'author' => 'required',
+                'year' => 'required|integer',
+                'category' => 'required',
+                'image' => 'required'
+            ],
+            [   
+                'title.required' => 'OBRIGATÓRIO SELECIONAR TITULO!',
+                'author.required' => 'OBRIGATÓRIO SELECIONAR AUTOR!',
+                'year.required' => 'OBRIGATÓRIO SELECIONAR ANO!',
+                'year.integer' => 'O ANO PRECISA SER UM NÚMERO INTEIRO!',
+                'category.required' => 'OBRIGATÓRIO SELECIONAR CATEGORIA!',
+                'image.required' => 'OBRIGATÓRIO SELECIONAR IMAGEM DO LIVRO!',
+            ]
+        );
+
+        $data = [
+            'title' => $request->input('title'),
+            'author_id' => $request->input('author'),
+            'year' => $request->input('year'),
+            'category_id' => $request->input('category'),
+            'image' => $request->file('image'),
+            'created_at' => Carbon::now('America/Sao_Paulo')
+        ];
+
+
+        $this->service->create($data);
+
+        return redirect()->route('home');
     }
 }
